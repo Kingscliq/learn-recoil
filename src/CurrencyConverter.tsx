be3458648d93b24440bfefce2ee05a7ee033a2c7
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-18 23:49:06 -0800

    Conversion of useState to recoil

--- src/CurrencyConverter.tsx
+++ src/CurrencyConverter.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil'
+
+const conversionRate = 0.83
+const usdAtom = atom({
+    key: 'usd',
+    default: 1
+})
+
+const eurSelector = selector({
+    key: "eur",
+    get: ({ get }) => get(usdAtom) * conversionRate
+})
 
 const CurrencyConverter = () => {
 
-    const conversionRate = 0.83
-    const [usd, setUsd] = useState<number>(0)
-    const [eur, setEur] = useState<number>(0)
+
+    // const [usd, setUsd] = useState<number>(0)
+    // const [eur, setEur] = useState<number>(0)
+
+    const [usd, setUsd] = useRecoilState(usdAtom)
+    const eur = useRecoilValue(eurSelector)
 
 
     console.log(eur)
@@ -12,7 +27,7 @@ const CurrencyConverter = () => {
         <div className=''>
             <div>
                 <input type="text" value={usd} onChange={(e) => setUsd(e.target.value as unknown as number)} placeholder='Enter amount' />
-                <input type="text" placeholder='Enter Amount' value={usd * conversionRate} onChange={(e) => setEur(e.target.value as unknown as number)} />
+                <input type="text" placeholder='Enter Amount' value={usd * conversionRate} />
             </div>
             <div><button>Calculate</button></div>
         </div>
