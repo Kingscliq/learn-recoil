91567a9ef734c21a84affea1711772da30d272ca
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-18 23:39:34 -0800

    Add: completion of recoil state

--- src/CurrencyConverter.tsx
+++ src/CurrencyConverter.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CurrencyConverter = () => {
+
+    const conversionRate = 0.83
+    const [usd, setUsd] = useState<number>(0)
+    const [eur, setEur] = useState<number>(0)
+
     return (
-        <div>CurrencyConverter</div>
+        <div className=''>
+            <div>
+                <input type="text" value={usd} onChange={(e) => setUsd(e.target.value as unknown as number)} placeholder='Enter amount' />
+                <input type="text" placeholder='Enter Amount' value={usd * conversionRate} onChange={(e) => setEur(e.target.value as unknown as number)} />
+            </div>
+            <div><button>Calculate</button></div>
+        </div>
     )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import CurrencyConverter from './CurrencyConverter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      {/* <App /> */}
+      <CurrencyConverter />
     </RecoilRoot>
   </React.StrictMode>
 );
